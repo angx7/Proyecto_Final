70ab351ef384ef6334610fa11439507ffbac61fc
Ángel Becerra <angelmirocu@gmail.com>
2023-11-26 14:28:28 -0600

    Uodate con jugadores

--- Ahorcado/Ahorcado.java
+++ Ahorcado/Ahorcado.java
@@ -4,16 +4,35 @@ import java.util.Scanner;
 
 public class Ahorcado {
     public static void main(String[] args) {/// Void
+        Scanner teclado = new Scanner(System.in);
+        int maxscore = 0;
+        int score1;
+        int score2 = 0;
+        System.out.print("Ingrese el nombre del jugador 1: ");
+        String j1 = teclado.next();
 
-        int fallosPermitidos = 7, fallos = 0, encontrados;
+        do {
+            score1 = jugador1();
+            score2 = jugador2();
+        } while (maxscore < 3);
+
+        if (score1 > score2) {
+            System.out.println("Gana el jugador 1\nFelicidades " + j1);
+        }
+
+    }
+
+    private static int jugador1() {
+        int puntuacion = 0;
+        int fallosPermitidos = 6, fallos = 0, encontrados;
         String letra, PalabraAdivinar, letrasEncontradas = "", Mensaje = "";
         boolean adivinado = false;
         Scanner teclado = new Scanner(System.in);
 
-        System.out.print("Introduce la palabra a adivinar: ");
-        PalabraAdivinar = teclado.next().toLowerCase();
-        letrasEncontradas = iniciarLetrasEncontradas(PalabraAdivinar.length());
         do {
+            System.out.print("Introduce la palabra a adivinar: ");
+            PalabraAdivinar = teclado.next().toLowerCase();
+            letrasEncontradas = iniciarLetrasEncontradas(PalabraAdivinar.length());
             System.out.print("Introduce una letra: ");
             letra = teclado.next().toLowerCase();
             encontrados = actualizarFallos(PalabraAdivinar, letra);
@@ -32,12 +51,53 @@ public class Ahorcado {
         } while (fallos < fallosPermitidos && adivinado == false);
 
         if (adivinado == false) {
-
             System.out.println("Lo siento, estás tonto. \n Perdiste bro");
         } else {
+            puntuacion++;
             System.out.println("Ganaste bro");
         }
         teclado.close();
+        return puntuacion;
+    }
+
+    private static int jugador2() {
+        int puntuacion = 0;
+        int fallosPermitidos = 6, fallos = 0, encontrados;
+        String letra, PalabraAdivinar, letrasEncontradas = "", Mensaje = "";
+        boolean adivinado = false;
+        Scanner teclado = new Scanner(System.in);
+        do {
+            System.out.print("Introduce la palabra a adivinar: ");
+            PalabraAdivinar = teclado.next().toLowerCase();
+            letrasEncontradas = iniciarLetrasEncontradas(PalabraAdivinar.length());
+            do {
+                System.out.print("Introduce una letra: ");
+                letra = teclado.next().toLowerCase();
+                encontrados = actualizarFallos(PalabraAdivinar, letra);
+                if (encontrados == 0) {
+                    fallos++;
+                }
+                Mensaje += "La palabra contiene " + encontrados + " '" + letra.charAt(0) + "' en la palabra buscada";
+                Mensaje += "\n" + dibujarAhorcado(fallos);
+                letrasEncontradas = actualizarLetrasEncontradas(PalabraAdivinar, letra, letrasEncontradas);
+                Mensaje += "\n letras encontradas: " + letrasEncontradas;
+                System.out.println(Mensaje);
+                Mensaje = "";
+                if (PalabraAdivinar.equals(letrasEncontradas)) {
+                    adivinado = true;
+                }
+            } while (fallos < fallosPermitidos && adivinado == false);
+
+            if (adivinado == false) {
+                System.out.println("Lo siento, estás tonto. \n Perdiste bro");
+            } else {
+                puntuacion++;
+                System.out.println("Ganaste bro");
+            }
+        } while (adivinado);
+
+        teclado.close();
+        return puntuacion;
     }
 
     private static String iniciarLetrasEncontradas(int longitud) {
@@ -58,7 +118,6 @@ public class Ahorcado {
         return encontrado;
     }
 
-    // Modificando ando
     private static String actualizarLetrasEncontradas(String PalabraAdivinar, String letra, String letrasEncontradas) {
         String actualizacionLetras = "";
         for (int i = 0; i < PalabraAdivinar.length(); i++) {
