fbf369769984195fa3568f8810ddf51c3149d4f8
Ángel Becerra <angelmirocu@gmail.com>
2023-11-26 17:09:40 -0600

    User Experience

--- Ahorcado/Ahorcado.java
+++ Ahorcado/Ahorcado.java
@@ -5,9 +5,68 @@ import java.util.Scanner;
 public class Ahorcado {
     public static void main(String[] args) {/// Void
         Scanner teclado = new Scanner(System.in);
+        String defaultcolor = "\u001B[37m";
         int maxscore = 0;
         int score1 = 0;
         int score2 = 0;
+        limpiarConsola();
+        // Instrucciones de juego
+        System.out.println("\u001B[36m"
+                + " ----------------------------------------------------------------------------------------------------------------------------------\r\n"
+                +
+                "\t\t\tBienvenido al juego del ahorcado\n\n\t\t\t\tInstrucciones\n\n * Elige la temática del juego\r\n"
+                +
+                " * Ingresa el nombre de los jugadores\r\n" +
+                " * Cada jugador tiene un máximo de 6 intentos para adivinar la palabra\r\n" +
+                " * Inicia el jugador 1 a adivinar la palabra aleatoria según la temática elegida\r\n" +
+                " * El jugador en turno solo puede poner una letra por intento\r\n" +
+                " * Si adivina la palabra seguira jugando el mismo jugador y se le sumara un punto, pero si falla será turno del siguiente\r\n"
+                +
+                " **Nota: Las palabras no contienen espacios\r\n" +
+                " * Gana el primero en llegar a 3 puntos\r\n\n"
+                + " ----------------------------------------------------------------------------------------------------------------------------------\r\n"
+                + defaultcolor);
+
+        System.out.println("Presiona enter para continuar...");
+        teclado.nextLine();
+        limpiarConsola();
+
+        // Menú de inicio
+        System.out.println("\u001B[31m"
+                + " -------------------------------------------------------------------------------------\r\n"
+                +
+                "\t\t\t\tMenú\n\n 1. Iniciar\r\n" +
+                " 2. Cerrar\r\n\n"
+                + " -------------------------------------------------------------------------------------\r\n"
+                + defaultcolor);
+        int op = teclado.nextInt();
+
+        switch (op) {
+            case 1:
+                limpiarConsola();
+                System.out.println("\u001B[32m"
+                        + " -------------------------------------------------------------------------------------\r\n"
+                        +
+                        "\t\t\t\tMenú de temáticas\n\n 1. Ciudades de México\r\n" +
+                        " 2. Animales\r\n 3. Frutas\r\n 4. Nombres de personas\r\n 5. Sitios Web\r\n\n"
+                        + " -------------------------------------------------------------------------------------\r\n"
+                        + defaultcolor);
+                break;
+            case 2:
+                limpiarConsola();
+                System.out.println("Gracias por jugar\n");
+                System.out.println("Presiona enter para continuar...");
+                teclado.nextLine();
+                limpiarConsola();
+                System.out.println(
+                        "\t\t\t\tCréditos\n\n* Angel Alejandro Becerra Rojas\r\n* Christian Axel Moreno Flores\r\n* Andrés Aguilera");
+                System.out.println("Presiona enter para continuar...");
+                teclado.nextLine();
+                limpiarConsola();
+                System.exit(0);
+                break;
+        }
+
         System.out.print("Ingrese el nombre del jugador 1: ");
         String j1 = teclado.next();
         System.out.print("Ingrese el nombre del jugador 2: ");
@@ -34,6 +93,11 @@ public class Ahorcado {
         }
     }
 
+    private static void limpiarConsola() {
+        System.out.print("\033[2J");
+        System.out.print("\033[H");
+    }
+
     private static int jugador1(String j1, String j2, int score1, int score2) {
         int puntuacion = score1;
         int fallosPermitidos = 6, fallos = 0, encontrados;
@@ -54,7 +118,7 @@ public class Ahorcado {
                     fallos++;
                 }
                 Mensaje += "La palabra contiene " + encontrados + " '" + letra.charAt(0) + "' en la palabra buscada";
-                Mensaje += "\n" + dibujarAhorcado(fallos);
+                Mensaje += "\n" + "Te restan " + (6 - fallos) + " intentos";
                 letrasEncontradas = actualizarLetrasEncontradas(PalabraAdivinar, letra, letrasEncontradas);
                 Mensaje += "\n letras encontradas: " + letrasEncontradas;
                 System.out.println(Mensaje);
@@ -98,9 +162,9 @@ public class Ahorcado {
                     fallos++;
                 }
                 Mensaje += "La palabra contiene " + encontrados + " '" + letra.charAt(0) + "' en la palabra buscada";
-                Mensaje += "\n" + dibujarAhorcado(fallos);
+                Mensaje += "\n" + "Te restan " + (6 - fallos) + " intentos";
                 letrasEncontradas = actualizarLetrasEncontradas(PalabraAdivinar, letra, letrasEncontradas);
-                Mensaje += "\n letras encontradas: " + letrasEncontradas;
+                Mensaje += "\nletras encontradas: " + letrasEncontradas;
                 System.out.println(Mensaje);
                 Mensaje = "";
                 if (PalabraAdivinar.equals(letrasEncontradas)) {
