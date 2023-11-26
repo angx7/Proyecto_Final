785aa07bdb53ace4e7af3e0bf2175fc270e8518b
Ángel Becerra <angelmirocu@gmail.com>
2023-11-26 13:56:51 -0600

    Update

--- Ahorcado/Ahorcado.java
+++ Ahorcado/Ahorcado.java
@@ -5,30 +5,22 @@ import java.util.Scanner;
 public class Ahorcado {
     public static void main(String[] args) {/// Void
 
-        int fallosPermitidos = 7;
-        int fallos = 0;
-        // int i;
-        int encontrados;
-        String letra;
-        String PalabraAdivinar;
-        String letrasEncontradas = "";
-        String Mensaje = "";
+        int fallosPermitidos = 7, fallos = 0, encontrados;
+        String letra, PalabraAdivinar, letrasEncontradas = "", Mensaje = "";
         boolean adivinado = false;
         Scanner teclado = new Scanner(System.in);
 
-        System.out.println("Introduce la palabra a adivinar");
-        PalabraAdivinar = teclado.next();
-        PalabraAdivinar = PalabraAdivinar.toLowerCase();
+        System.out.print("Introduce la palabra a adivinar: ");
+        PalabraAdivinar = teclado.next().toLowerCase();
         letrasEncontradas = iniciarLetrasEncontradas(PalabraAdivinar.length());
         do {
-            System.out.println("Introduce una letra");
-            letra = teclado.next();
-            letra = letra.toLowerCase();
+            System.out.print("Introduce una letra: ");
+            letra = teclado.next().toLowerCase();
             encontrados = actualizarFallos(PalabraAdivinar, letra);
             if (encontrados == 0) {
                 fallos++;
             }
-            Mensaje += "La palabra contiene " + encontrados + "" + letra.charAt(0) + " en la palabra buscada";
+            Mensaje += "La palabra contiene " + encontrados + " '" + letra.charAt(0) + "' en la palabra buscada";
             Mensaje += "\n" + dibujarAhorcado(fallos);
             letrasEncontradas = actualizarLetrasEncontradas(PalabraAdivinar, letra, letrasEncontradas);
             Mensaje += "\n letras encontradas: " + letrasEncontradas;
@@ -50,18 +42,15 @@ public class Ahorcado {
 
     private static String iniciarLetrasEncontradas(int longitud) {
         String letras = "";
-        int i;
-        for (i = 0; i < longitud; i++) {
+        for (int i = 0; i < longitud; i++) {
             letras = letras + 0;
-
         }
         return letras;
     }
 
     private static int actualizarFallos(String PalabraAdivinar, String letra) {
-        int i;
         int encontrado = 0;
-        for (i = 0; i < PalabraAdivinar.length(); i++) {
+        for (int i = 0; i < PalabraAdivinar.length(); i++) {
             if (PalabraAdivinar.charAt(i) == letra.charAt(0)) {
                 encontrado++;
             }
@@ -71,9 +60,8 @@ public class Ahorcado {
 
     // Modificando ando
     private static String actualizarLetrasEncontradas(String PalabraAdivinar, String letra, String letrasEncontradas) {
-        int i;
         String actualizacionLetras = "";
-        for (i = 0; i < PalabraAdivinar.length(); i++) {
+        for (int i = 0; i < PalabraAdivinar.length(); i++) {
             if (letra.charAt(0) == PalabraAdivinar.charAt(i)) {
                 actualizacionLetras += letra.charAt(0);
             } else {
